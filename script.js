147869e3329dac26911a6d544515945a2be4d417
SanglierKevin <sanglierkev@gmail.com>
2022-11-09 17:40:35 +0100

    Images fonctionnent

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function loadOther() {
-    var imageFileAlt = "media/images/1.png";
-    document.getElementById("imageID").src = imageFileAlt;
-}
-
 var date = new Date();
 var dd = date.getDate();
 var mm = date.getMonth() + 1;
 var yyyy = date.getFullYear();
 var imageFile = "media/images/" + dd + "-" + mm + "-" + yyyy + ".png";
 
+$.get(imageFile)
+    .done(function() {
+        console.log(imageFile + " existe");
+        document.getElementById("imageID").src = imageFile;
+    }).fail(function() {
+        console.log(imageFile + " existe pas");
+        var imageFile = "/media/images/" + "1" + ".png";
+        document.getElementById("imageID").src = imageFile;
+    })
