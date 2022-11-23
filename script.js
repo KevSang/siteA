0668d24e458dc050672970296022ff8634a9bc88
SanglierKevin <sanglierkev@gmail.com>
2022-11-23 15:56:55 +0100

    vidéos ytb

--- script.js
+++ script.js
@@ -2,14 +2,41 @@ var date = new Date();
 var dd = date.getDate();
 var mm = date.getMonth() + 1;
 var yyyy = date.getFullYear();
-var imageFile = "media/images/" + dd + "-" + mm + "-" + yyyy + ".png";
+var fullDate = dd + "-" + mm + "-" + yyyy;
+var imageFile = "media/images/" + fullDate + ".png";
+var linkFile = "media/links/" + fullDate + ".txt";
+var textFile = "media/texts/" + fullDate + ".txt";
+
+function getRandom(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 $.get(imageFile)
     .done(function() {
-        console.log(imageFile + " existe");
         document.getElementById("imageID").src = imageFile;
     }).fail(function() {
-        console.log(imageFile + " existe pas");
-        var imageFile = "media/images/" + "1" + ".png";
+        var imageFile = "media/images/R" + getRandom(0,1) + ".png";
         document.getElementById("imageID").src = imageFile;
+    })
+
+$.get(linkFile)
+    .done(function() {
+        $.get(linkFile, function(data) {
+            var ytbLink = "https://www.youtube.com/embed/" + data;
+            document.getElementById("linkID").src = ytbLink;
+        });
+    }).fail(function() {
+        var linkFile = "media/links/R" + getRandom(0,1) + ".txt";
+        $.get(linkFile, function(data) {
+            var ytbLink = "https://www.youtube.com/embed/" + data;
+            document.getElementById("linkID").src = ytbLink;
+        });
+    })
+
+$.get(textFile)
+    .done(function() {
+        document.getElementById("textID").src = textFile;
+    }).fail(function() {
+        var textFile = "media/texts/R" + getRandom(0,1) + ".txt";
+        document.getElementById("textID").src = textFile;
     })
